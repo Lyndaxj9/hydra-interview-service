46f15de8f69e9eab490b2a3b2e24d3eb7cd022a4
Lynda Ademola <lyndaxj9@gmail.com>
2018-03-22 09:46:17 -0400

    added testing methods

--- src/test/java/com/revature/hydra/interview/InterviewRepositoryTest.java
+++ src/test/java/com/revature/hydra/interview/InterviewRepositoryTest.java
@@ -1,34 +1,36 @@
 package com.revature.hydra.interview;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.beans.Interview;
+import com.revature.hydra.interview.application.InterviewRepositoryServiceApplication;
+import com.revature.hydra.interview.data.InterviewRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = InterviewRepositoryServiceApplication.class)
 public class InterviewRepositoryTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	@Autowired
+	InterviewRepository test;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindAll() {
+		List<Interview> interviews = test.findAll();
+		assertNotNull(interviews);
+	}
+	
+	@Test
+	public void testFindAllByAssociateId() {
+		List<Interview> interviews = test.findAllByAssociateId(5501);
+		assertNotNull(interviews);
 	}
 
 }
